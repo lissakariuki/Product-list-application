6f519e42190ad5581438f06add0ecd88c60583bd
Melissa Kariuki <Melissa.Kariuki@imbank.co.ke>
2025-08-21 15:39:15 +0300

    Removing provided import common module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.scss'
 })
